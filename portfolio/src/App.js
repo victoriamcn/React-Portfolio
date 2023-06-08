9346a276e2a8282ad811b673a358f98bbea7a961
Victoria <vmcnorrill@gmail.com>
2023-06-08 19:48:24 -0400

    created all components

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -2,15 +2,17 @@ import './App.css';
 
 //components
 import Header from "./components/Header";
+import Navigation from "./components/Navigation"
 import Projects from "./components/Projects";
 import Footer from "./components/Footer"
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Projects/>
-      <Footer/>
+      <Header />
+      <Navigation />
+      <Projects />
+      <Footer />
     </div>
   );
 }
