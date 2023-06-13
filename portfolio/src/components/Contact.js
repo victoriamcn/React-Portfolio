c8846edd8ca390269126773a71b6b81511700de9
Victoria <vmcnorrill@gmail.com>
2023-06-12 20:39:42 -0400

    BUG: arsing error: Identifier 'value' has already been declared.

--- portfolio/src/components/Contact.js
+++ portfolio/src/components/Contact.js
@@ -1,61 +1,56 @@
-import React  from 'react';
+import React from 'react';
+import { useState } from 'react';
 
-//{ useState, useEffect  }
+export default function Contact() {
+    const [formData, setFormData] = useState({ name: "", email: "", message: "" });
 
-// function ContactForm(props) {
-//     const [formData, setFormData] = useState({
-//         name: "",
-//         email: "",
-//         message: ""
-//     });
-    
-//     let [name, setName] = useState(
-//         function handleNameInput(event) {
-//             setName({ ...name, name: e.target.value });
-//         }
-//     ) 
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        const { email, value } = event.target;
+        const { message, value } = event.target;
+        setFormData((prevFormData) => (
+            { ...prevFormData, [name]: value },
+            { ...prevFormData, [email]: value },
+            { ...prevFormData, [message]: value }
+        ));
+    };
 
-//     let [email, setEmail] = useState("")
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(`Name: ${formData.name}, Email: ${formData.email}, Message: ${formData.message}`
+        )
+    };
 
-//     let [message, setMessage] = useState("")
 
-//     const handleSubmit = (event) => {
-//         event.preventDefault()
-//         console.log(formData)
-//     }
-// }
-
-const Contact = () => {
-    return (
-        <section id="contact" className="content" >
-            {/*onSubmit={handleSubmit}*/}
-            <form className="form box">
-            <p className="title">Contact Me</p>
-                <div className="field">
-                    <label className="label">Name:</label>
-                    <div className="control">
-                        <input className="input" type="text" placeholder="e.g Anne Elliot" required/>
+    const Contact = () => {
+        return (
+            <section id="contact" className="content" >
+                <form className="form box">
+                    <p className="title">Contact Me</p>
+                    <div className="field">
+                        <label className="label">Name:</label>
+                        <div className="control">
+                            <input id="name" name="name" value={formData.name} onChange={handleChange} className="input" type="text" placeholder="e.g Anne Elliot" required />
+                        </div>
                     </div>
-                </div>
 
-                <div className="field">
-                    <label className="label">Email:</label>
-                    <div className="control">
-                        <input className="input"  type="email" placeholder="e.g. anne@gmail.com" required />
+                    <div className="field">
+                        <label className="label">Email:</label>
+                        <div className="control">
+                            <input id="email" name="email" value={formData.email} onChange={handleChange} className="input" type="email" placeholder="e.g. anne@gmail.com" required />
+                        </div>
                     </div>
-                </div>
 
-                <label className="label">Say something:</label>
-                <div className="control">
-                    <textarea className="textarea" placeholder="e.g. Something"></textarea>
-                </div>
+                    <label className="label">Say something:</label>
+                    <div className="control">
+                        <textarea id="message" name="message" value={formData.message} onChange={handleChange} className="textarea" placeholder="e.g. Something"></textarea>
+                    </div>
 
-                <div className="control">
-                    <button className="button submit m-3">Submit</button>
-                </div>
-            </form>
-        </section>
-    )
+                    <div className="control">
+                        <button className="button submit m-3">Submit</button>
+                    </div>
+                </form>
+            </section>
+        )
+    }
 }
-
-export default Contact
