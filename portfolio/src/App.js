d01e7a86a403bdcfdd28d22d8064a4e80a8084fd
Victoria <vmcnorrill@gmail.com>
2023-06-14 19:21:27 -0400

    BUG: only page that I can see is About, cannot select another page

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,22 +1,16 @@
 //router
 import React from 'react'
-//import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 
 // styling
 import './App.css';
 
 //components
-import Header from "./components/Header";
-import Main from "./components/Main"
-import Footer from "./components/Footer";
+import PortfolioShell from "./components/PortfolioShell"
 
 function App() {
   return (
     <div className="App all-content">
-        <Header />
-        <Main className="container is-fluid"/>
-        <Footer />
+        <PortfolioShell />
     </div>
   );
 }
---
+++ portfolio/src/components/PortfolioShell.js
@@ -0,0 +1,80 @@
+import React, { useState } from 'react';
+
+import Navigation from "./Navigation"
+import Projects from "./pages/Projects";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Resume from "./pages/Resume";
+
+ function PortfolioShell() {
+  // About Page selected  by default
+  const [currentPage, setCurrentPage] = useState('About');
+
+  // This method is checking to see what the value of `currentPage` is. 
+  // Depending on the value of currentPage,
+  // we return the corresponding component to render.
+  const renderPage = () => {
+    if (currentPage === 'Projects') {
+      return <Projects />;
+    }
+    if (currentPage === 'About') {
+      return <About />;
+    }
+    if (currentPage === 'Resume') {
+      return <Resume />;
+    }
+    return <Contact />;
+  };
+
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+
+  return (
+    <div>
+      <header>
+        <Navigation currentPage={currentPage} handlePageChange={handlePageChange}/>
+      </header>
+
+
+      <main className="container">
+        {renderPage()}
+      </main>
+
+
+      <footer className="footer">
+      <div className="content has-text-centered">
+        <a href="https://github.com/victoriamcn" target="_blank" rel="noreferrer">
+          {/*<span className="icon">
+            <i className="fas-brands fa-github"></i>
+          </span>*/}
+          <span className="social">GitHub</span>
+        </a>
+        <a href="https://www.linkedin.com/in/victoria-mcnorrill/" target="_blank" rel="noreferrer">
+          {/*<span className="icon">
+            <i className="fas-brands fa-linkedin"></i>
+            </span>*/}
+          <span className="social">LinkedIn</span>
+        </a>
+        <a href="https://www.instagram.com/victoriamcnorrill/" target="_blank" rel="noreferrer">
+          {/*<span className="icon">
+            <i className="fas-brands fa-instagram"></i>
+          </span>*/}
+          <span className="social">Instagram</span>
+        </a>
+        <a href="https://open.spotify.com/user/torilizabeth95" target="_blank" rel="noreferrer">
+          {/*<span className="icon">
+            <i className="fas-brands fa-spotify"></i>
+          </span>*/}
+          <span className="social">Spotify</span>
+        </a>
+      </div>
+      <div className="content has-text-centered">
+        <p className='byvictoria'>by Victoria</p>
+      </div>
+    </footer>
+    </div>
+  );
+}
+
+export default PortfolioShell
