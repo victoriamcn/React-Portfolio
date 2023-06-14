59773222191928bb75c6e34484bbe9bf19a9d82d
Victoria <vmcnorrill@gmail.com>
2023-06-14 00:05:27 -0400

    got contact form working, need to validate email

--- portfolio/src/components/Contact.js
+++ portfolio/src/components/Contact.js
@@ -3,60 +3,90 @@ import { useState } from 'react';
 import { useForm } from "react-hook-form";
 
 
-
 const Contact = () => {
-const [formData, setFormData] = useState({ name: "", email: "", message: "" });
-const { register, handleSubmit, formState:{errors} } = useForm();
+    // Here we set two state variables for firstName and lastName using `useState`
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
 
+    const handleInputChange = (event) => {
+        // Getting the value and name of the input which triggered the change
+        const { target } = event;
+        const inputType = target.name;
+        const inputValue = target.value;
 
-const handleChange = (event) => {
-    const { name, value } = event.target;
-    // const { email, value } = event.target;
-    //const { message, value } = event.target;
-    setFormData((prevFormData) => (
-        { ...prevFormData, [name]: value }
-        //{ ...prevFormData, [email]: value },
-        //{ ...prevFormData, [message]: value }
-    ));
-};
+        // Based on the input type, we set the state of either email, name, and message
+        if (inputType === 'email') {
+            setEmail(inputValue);
+        } else if (inputType === 'name') {
+            setName(inputValue);
+        } else {
+            setMessage(inputValue)
+        }
+    };
 
-const onSubmit = (event) => {
-    event.preventDefault();
-    console.log(`Name: ${formData.name}, Email: ${formData.email}, Message: ${formData.message}`
-        // USING dotenv: on submit, email me
-    )
-}
+    const handleFormSubmit = (event) => {
+        // Preventing the default behavior of the form submit (which is to refresh the page)
+        event.preventDefault();
+
+        // email regex validation
+        const isEmail = (email) =>
+            /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email);
+
+        // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
+        if (!isEmail(email)) {
+            setErrorMessage('Email is invalid.');
+            // We want to exit out of this code block if something is wrong so that the user can correct it
+            return;
+        }
+
+        // Alert the person wanting to contact me that the message has been sent
+        alert(`Thank you, ${name}! I will get back to you ASAP with the email you provided.`);
+        setName('');
+        setEmail('');
+        setMessage('');
+    };
 
 
     return (
         <section id="contact" className="content" >
-            <form className="form box" onSubmit={handleSubmit(onSubmit)} action="mailto:vmcnorrill@gmail.com" method="post" enctype="text/plain">
+            <form className="form box">
                 <p className="title">Contact Me</p>
                 <div className="field">
                     <label className="label">Name:</label>
                     <div className="control">
-                        <input id="name" value={formData.name} onChange={handleChange} name="name" {...register("name", { required: true })} type="text" placeholder="e.g Anne Elliot" required />
-                        {errors.name && <p>Name is required</p>}
+                        <input
+                            id="name"
+                            value={name}
+                            onChange={handleInputChange}
+                            name="name"
+                            type="text"
+                            placeholder="e.g Anne Elliot"
+                            required />
                     </div>
                 </div>
 
                 <div className="field">
                     <label className="label">Email:</label>
                     <div className="control">
-                        <input id="email" value={formData.email} onChange={handleChange} name="email" {...register("email", { required: true, pattern: /^\S+@\S+$/i  })} type="email" placeholder="e.g. anne@gmail.com" required />
-                        {errors.email && <p>Email is required. Please submit valid email.</p>}
+                        <input id="email" value={email} onChange={handleInputChange} name="email" type="email" placeholder="e.g. anne@gmail.com" required />
                     </div>
                 </div>
 
                 <label className="label">Say something:</label>
                 <div className="control">
-                    <textarea id="message" value={formData.message} onChange={handleChange} name="textarea" placeholder="e.g. Something"></textarea>
-                    {errors.message && <p>Message is required.</p>}
+                    <textarea id="message" value={message} onChange={handleInputChange} name="textarea" placeholder="e.g. Something" rows="5" cols="30" required></textarea>
                 </div>
 
                 <div className="control">
-                    <button type="submit" name="submit" value="Submit" className="button submit m-3" id="submit">Submit</button>
+                    <button type="button" className="button submit m-3" onClick={handleFormSubmit}>Submit</button>
                 </div>
+                {errorMessage && (
+                <div>
+                    <p className="error-text">{errorMessage}</p>
+                </div>
+            )}
             </form>
         </section>
     )
