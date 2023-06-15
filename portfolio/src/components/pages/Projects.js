7b72369a92393139652f559fe7625c898678378e
Victoria <vmcnorrill@gmail.com>
2023-06-14 23:02:00 -0400

    images are not loading in react

--- portfolio/src/components/pages/Projects.js
+++ portfolio/src/components/pages/Projects.js
@@ -1,17 +1,18 @@
 import React from 'react';
 
-// import LaborHubImg from "../../assets/images/LaborHub-desktop-dashboard.jpg";
-// import NightOwlImg from "../../assets/images/NightOwl-Home.html.png";
-// import SocialNetImg from "../../assets/images/nosqlsocialnetworkPUTroute.png";
-// import WeatherImg from "../../assets/images/weatherpreview.png";
-// import TimedQuizImg from "../../assets/images/Timed-Quiz-Preview.png"
+import LaborHubImg from "../../assets/images/LaborHub-desktop-dashboard.jpg";
+import NightOwlImg from "../../assets/images/NightOwl-Home.html.png";
+import SocialNetImg from "../../assets/images/nosqlsocialnetworkPUTroute.png";
+import WeatherImg from "../../assets/images/weatherpreview.png";
+import TimedQuizImg from "../../assets/images/Timed-Quiz-Preview.png"
+import ReactPortfolio from "../../assets/images/reactportfolio.png"
 
 const list = [
     {
         title: 'LaborHub',
         repository: 'https://github.com/victoriamcn/LaborHub',
         deployed: "https://laborhub.herokuapp.com/",
-        //preview: "LaborHub-desktop-dashboard.jpg",
+        preview: {LaborHubImg },
         alt: "LaborHub App. the navigation has links to the Homepage, User Dashboard, Pay Dues and Logout. The user dashboard says 'Welcome, Test User.' It has sections for dues, a calendar, a union representative and messages.",
         description: "LaborHub is an app that allows users to share their wages, demographics data, and check if dues are paid.",
         tech: 'MySQL, Express.JS, Handlebars.JS, Node.JS, Sequelize, dotenv, npm, JavaScript, Spectre.css, CSS',
@@ -20,7 +21,7 @@ const list = [
         title: 'NightOwl',
         repository: 'https://github.com/victoriamcn/NightOwl',
         deployed: "https://victoriamcn.github.io/NightOwl/",
-        //preview: { NightOwlImg },
+        preview: { NightOwlImg },
         alt: "NightOwl application. Text says: Find your next adventure! Simply enter your criteria below and click 'search'. User can type in their city and select a date. Then the user can check if they want to search for music, sports or theatre events.",
         description: "NightOwl is an application designed to make event and dining recommendations based on the user's location and selected preferences.",
         tech: 'Third Party API, day.js, localStorage, Bootstrap, Javascript, HTML, CSS',
@@ -29,7 +30,7 @@ const list = [
         title: 'NoSQL Social Network',
         repository: 'https://github.com/victoriamcn/NoSQL-Social-Network',
         deployed: "https://www.youtube.com/watch?v=CzLSilbr5A0",
-        //preview: { SocialNetImg },
+        preview: { SocialNetImg },
         alt: "Insomnia used to test a GET route to retrieve all users from the database",
         description: "This is an API for a social network web application. Users can share thoughts, react and create a friend list.",
         tech: "MongoDB, Mongoose, Express.JS, dotenv, Node.JS, npm, JavaScript",
@@ -38,7 +39,7 @@ const list = [
         title: 'Weather Dashboard',
         repository: 'https://github.com/victoriamcn/Weather-Dashboard',
         deployed: "https://github.com/victoriamcn/Weather-Dashboard",
-        //preview: { WeatherImg },
+        preview: { WeatherImg },
         alt: "screenshot of a weather application that displays the current day's and the five following days' forecast",
         description: "A simple weather application that runs in the browser and utilizes the 5 Day Weather Forecast API to retrieve weather data for cities.",
         tech: 'Third Party API, localStorage, Javascript, HTML, CSS',
@@ -47,7 +48,7 @@ const list = [
         title: 'Timed Quiz',
         repository: 'https://github.com/victoriamcn/Timed-Coding-Quiz',
         deployed: "https://victoriamcn.github.io/Timed-Coding-Quiz/",
-        //preview: { TimedQuizImg },
+        preview: { TimedQuizImg },
         alt: 'screenshot of the start window for a coding quiz',
         description: "This quiz application incorporates local storage to save and retrieve users' scores. After completing the quiz, the user's score and initials are displayed with other players' scores.",
         tech: 'localStorage, JavaScript, HTML, CSS',
@@ -56,8 +57,8 @@ const list = [
         title: 'Portfolio with React',
         repository: "https://github.com/victoriamcn/React-Portfolio",
         deployed: "",
-        preview: {},
-        alt: "",
+        preview: { ReactPortfolio },
+        alt: "HEader: Victoria McNorrill, Full Stack Web Developer. NAvigation: Projects, About Resume, Contact. Resume Page is active. Footer: links to Github, LinkedIn, Spotify.",
         description: "My tech portfolio is built with React and designed to showcase my web development skills to potential employers.",
         tech: 'JavaScript, React, Bulma.css',
     },
@@ -71,6 +72,9 @@ function Projects() {
         {list.map((detail, i) => (
             <article className="box column is-one-third projectcol m-1">
                 <p data-value={detail.title} className="title">{detail.title}</p>
+                <figure>
+                    <img src={detail.preview} alt={detail.alt}/>
+                </figure>
                 <p data-value={detail.description} className="text">{detail.description}</p>
                 <div className="level is-mobile">
                     <div className="level-item has-text-centered">
