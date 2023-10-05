865c6518515599b8a9263dd090843c3c60c80d2f
Victoria <vmcnorrill@gmail.com>
2023-10-05 18:18:54 -0400

    combined past/current projects pages to be on one page

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -9,7 +9,7 @@ import ReactPortfolio from "../../assets/images/reactportfolio.png"
 import OCDetourImg from "../../assets/images/activitypreview.png"
 import PixieCutsBySarahIMG from "../../assets/images/PixiecutsandfairyustbysarahIMG.png"
 
-const list = [
+const finishedProjects = [
     {
         title: 'Pixie Cuts and Fairy Dust',
         //repository: 'https://github.com/jsnicholas/OCDetour',
@@ -92,34 +92,81 @@ const list = [
     },
 ]
 
+const currentProjects = [
+    {
+        title: 'Jones Landscaping Services',
+        link: 'link',
+        //repository: 'link',
+        // deployed: '',
+        // preview: Img,
+        // alt: 'put alt here',
+        description: 'Jones Landscaping Service is a business based out of Abbeville, SC.',
+        // tech: 'SquareSpace, Wix, Web Design',
+        key: 9,
+    },
+    {
+        // title: 'title',
+        // repository: 'link',
+        // deployed: 'link',
+        // preview: Img,
+        // alt: 'alt',
+        // description: 'description',
+        // // tech: '',
+        // key: number,
+    },
+]
+
 function Projects() {
     return (
-         <section id="projects" className="flexcards content">   
-                 <div className="columns is-flex is-flex-wrap-wrap is-justify-content-center">
-        <p></p>
-        {list.map((detail, i) => (
-            <article className="box column is-one-third projectcol m-1" key={detail.key}>
-                <p data-value={detail.title} className="title">{detail.title}</p>
-                <figure>
-                    <img src={detail.preview} alt={detail.alt}/>
-                </figure>
-                <p data-value={detail.description} className="text">{detail.description}</p>
-                <div className="level is-mobile">
-                    <div className="level-item has-text-centered">
-                        <div>
-                            <a href={detail.repository} target="_blank" rel="noreferrer" ><button className="button is-small" data-value={detail.repository}  >Repository</button></a>
+        <div>
+                <section id="projects" className="flexcards content">   
+                <div className="columns is-flex is-flex-wrap-wrap is-justify-content-center">
+                <p></p>
+                    {currentProjects.map((detail, i) => (
+                        <article className="box column is-one-third projectcol m-1" key={detail.key}>
+                        <p data-value={detail.title} className="title">{detail.title}</p>
+                        {/* <figure>
+                        <img src={detail.preview} alt={detail.alt}/>
+                        </figure> */}
+                        <p data-value={detail.description} className="text">{detail.description}</p>
+                        <div className="level is-mobile">
+                        <div className="level-item has-text-centered">
+                            <div>
+                                <a href={detail.link} target="_blank" rel="noreferrer" ><button className="button is-small" data-value={detail.link}  >Check It Out!</button></a>
+                            </div>
+                        </div>
+                        </div>
+                    </article>
+                    ))}
+                </div>;
+            </section >
+        <section id="projects" className="flexcards content">   
+            <div className="columns is-flex is-flex-wrap-wrap is-justify-content-center">
+            <p></p>
+            {finishedProjects.map((detail, i) => (
+                <article className="box column is-one-third projectcol m-1" key={detail.key}>
+                    <p data-value={detail.title} className="title">{detail.title}</p>
+                    <figure>
+                        <img src={detail.preview} alt={detail.alt}/>
+                    </figure>
+                    <p data-value={detail.description} className="text">{detail.description}</p>
+                    <div className="level is-mobile">
+                        <div className="level-item has-text-centered">
+                            <div>
+                                <a href={detail.repository} target="_blank" rel="noreferrer" ><button className="button is-small" data-value={detail.repository}  >Repository</button></a>
+                            </div>
+                        </div>
+                        <div className="level-item has-text-centered">
+                            <div>
+                                <a href={detail.deployed} target="_blank" rel="noreferrer" ><button className="button is-small" data-value={detail.deployed} >Application</button></a>
+                            </div>
                         </div>
                     </div>
-                    <div className="level-item has-text-centered">
-                        <div>
-                            <a href={detail.deployed} target="_blank" rel="noreferrer" ><button className="button is-small" data-value={detail.deployed} >Application</button></a>
-                        </div>
-                    </div>
-                </div>
-            </article>
-        ))}
-    </div>;
+                </article>
+            ))}
+            </div>;
         </section >
+        </div>
     )
 }
 
